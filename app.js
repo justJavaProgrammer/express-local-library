07d3d2e37809cfaf989580aed104d05a5ff5ede8
odeyalo <theprogrammerinfuture@gmail.com>
2025-04-18 22:11:51 +0300

    Add support for routes

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const queriesRouter = require("./routes/queries");
+const catalogRouter = require("./routes/catalog")
+
 
 const connectDB = require("./db/db")
-
 var app = express();
 
 connectDB()
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/queries', queriesRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
