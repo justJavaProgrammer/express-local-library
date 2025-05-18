8f6284e7cb42be61cdd3066058a3cb46212967e3
odeyalo <theprogrammerinfuture@gmail.com>
2025-05-18 15:37:19 +0300

    Prepare application for deploy

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog")
 
+require('dotenv').config()
 
+const helmet = require("helmet");
+const compression = require("compression");
 const connectDB = require("./db/db")
 var app = express();
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 хвилина
+    max: 20,
+});
 
 connectDB()
 
@@ -18,11 +27,21 @@ connectDB()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(limiter);
+app.use(compression())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+      },
+    }),
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
