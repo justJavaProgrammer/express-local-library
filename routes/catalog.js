0041490c6151056bef034319b440f18716cdd9b7
odeyalo <theprogrammerinfuture@gmail.com>
2025-05-18 16:01:45 +0300

    Change bookinstanceController bookInstanceController

--- routes/catalog.js
+++ routes/catalog.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const book_controller = require("../controllers/bookController");
 const author_controller = require("../controllers/authorController");
 const genre_controller = require("../controllers/genreController");
-const book_instance_controller = require("../controllers/bookinstanceController");
+const book_instance_controller = require("../controllers/bookInstanceController");
 
 /// МАРШРУТИ КНИГ /////
 
